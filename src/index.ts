5ff50eedb30501625223cddf4c92d13c01e01099
Jos Visser <goleztrol@hotmail.com>
2022-01-29 00:24:16 +0100

    Player class, drawing tank sprites (rotate)

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,32 @@ let t: number = 1;
 let x: number;
 let y: number;
 
+class Player{
+  x: number; 
+  y: number;
+  angle: number;
+  speed: number;
+  steer: number;
+
+  constructor(x: number, y: number, angle: number, speed: number){
+    this.x = x;
+    this.y = y;
+    this.angle = angle;
+    this.speed = speed;
+    this.steer = 0;
+  }
+}
+
+class Game {
+  player: Player;
+
+  constructor() {
+    this.player = new Player(100, 100, 0, 60);
+  }
+}
+
+let game = new Game();
+
 function TIC(): void {
   // small trick to ensure that everything is loaded before init() call
   if (t === 1) {
@@ -26,8 +52,9 @@ function TIC(): void {
     x++;
   }
 
-  cls(13);
+  cls(0); //13);
   spr(1 + Math.floor((t % 60) / 30) * 2, x, y, 14, 3, 0, 0, 2, 2);
+  spr(256 + Math.floor((t % 90) / 12) * 2, game.player.x, game.player.y, 0, 1, 0, 0, 2, 2);
   print("HELLO WORLD!", 84, 84);
   t++;
 }
